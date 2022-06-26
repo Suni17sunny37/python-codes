002d450c57bd26fb8a43c694c1d3300b4b878c6a
Sunil Kumar Reddy <108225478+Suni17sunny37@users.noreply.github.com>
2022-06-26 20:06:00 +0530

    spirograph

---
+++ spirograph.py
@@ -0,0 +1,11 @@
+# python-codes
+import turtle as t
+t.bgcolor("black")
+t.pensize(2)
+t.speed(10)
+for i in range(6):
+  for color in ('red','magenta','blue','cyan','green','orange','yellow'):
+    t.color(color)
+    t.circle(100)
+    t.left(10)
+  t.hideturtle()
